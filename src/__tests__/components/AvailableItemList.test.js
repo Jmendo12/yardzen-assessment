499da2efefc7bcf47f71ba8bd0ccb772b5182119
Jmendo12 <jmendo.1024@gmail.com>
2021-10-27 22:59:26 -0700

    Available items will now be displayed in a detail list view

--- src/__tests__/components/AvailableItemList.test.js
+++ src/__tests__/components/AvailableItemList.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { AvailableItemList } from 'components/AvailableItemList';
 
 it('renders without crashing', () => {
@@ -19,5 +19,75 @@ it('renders a message prompting the user to enter a budget', () => {
   const { getByText } = render(<AvailableItemList renderBudgetPrompt />);
 
   expect(getByText(/enter your budget/i)).toBeInTheDocument();
+});
+
+it('displays heading for each available item type', () => {
+  const { getByText } = render(
+    <AvailableItemList
+      items={[
+        {
+          type: "chairs",
+        },
+        {
+          type: "lighting"
+        }
+      ]}
+    />
+  );
+
+  expect(getByText("chairs")).toBeInTheDocument();
+
+  expect(getByText("lighting")).toBeInTheDocument();
+});
+
+it('displays a list of all available items for each type', () => {
+  const { getByText } = render(
+    <AvailableItemList
+      items={[
+        {
+          name: "test",
+          lowPrice: "100",
+          highPrice: "1000",
+          type: "chairs"
+        },
+        {
+          name: "second test",
+          lowPrice: "50",
+          highPrice: "250",
+          type: "sofas"
+        }
+      ]}
+    />
+  );
+
+  expect(getByText("test")).toBeInTheDocument();
+  expect(getByText("100")).toBeInTheDocument();
+  expect(getByText("1000")).toBeInTheDocument();
+
+  expect(getByText("second test")).toBeInTheDocument();
+  expect(getByText("50")).toBeInTheDocument();
+  expect(getByText("250")).toBeInTheDocument();
+});
+
+it('fires the geven select function when an item is selected', () => {
+  const onItemSelected = jest.fn();
+
+  const { getByLabelText } = render(
+    <AvailableItemList
+      items={[
+        {
+          name: "test",
+          lowPrice: "100",
+          highPrice: "1000",
+          type: "chairs"
+        }
+      ]}
+      onItemSelected={onItemSelected}
+    />
+  );
+
+  fireEvent.click(getByLabelText("test"));
+
+  expect(onItemSelected).toHaveBeenCalled();
 });
 
--- src/components/AvailableItemList.js
+++ src/components/AvailableItemList.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { BorderedContainerWithHeaderAndPromptMessage } from 'components/BorderedContainerWithHeaderAndPromptMessage';
+import { AvailableItemDetailList } from 'components/AvailableItemDetailList';
 
-export function AvailableItemList({ renderBudgetPrompt }) {
+export function AvailableItemList({ renderBudgetPrompt, items = [], onItemSelected = () => 0 }) {
+  /*
+    We'll first get an array of all types with duplicates, and then filter it so there are no duplicates.
+    We use an array instead of a set because we still need to map the types to components.
+  */
+  const itemTypes = useMemo(
+    () => (
+      items.map(item => item.type).filter((itemType, index, itemTypes) => itemTypes.indexOf(itemType) === index)
+    ),
+    [items]
+  );
+
   return (
     <BorderedContainerWithHeaderAndPromptMessage
       headerText="Available Items"
       renderPrompt={renderBudgetPrompt}
       promptText="Enter your budget above to begin selecting items"
-    />
+    >
+      {
+        itemTypes.map((type, index) => (
+          <AvailableItemDetailList
+            summaryText={type}
+            key={`item-detail-${type}-${index}`}
+            items={items.filter(item => item.type === type)}
+            onItemSelected={(e) => onItemSelected(e)}
+          />
+        ))
+      }
+    </BorderedContainerWithHeaderAndPromptMessage>
   );
 }
